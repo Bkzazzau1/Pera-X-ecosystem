426a49004e64a20c0198119af92bfeeeb8b96046
Bkzazzau1 <y.i.bashir@gmail.com>
2026-07-19 13:42:59 +0100

    Route product updates through immutable policy handler

--- perax-contracts/programs/perax-core/src/instructions/mod.rs
+++ perax-contracts/programs/perax-core/src/instructions/mod.rs
@@ -3,6 +3,7 @@ mod burn;
 mod config;
 mod counterweight;
 mod hardened_market;
+mod hardened_product;
 #[path = "../market_cpi.rs"]
 mod market_cpi;
 mod payments;
@@ -18,6 +19,7 @@ pub use hardened_market::{
     execute_counterweight_purchase_hardened as execute_counterweight_purchase,
     execute_settlement_market_purchase_hardened as execute_settlement_market_purchase,
 };
+pub use hardened_product::update_product_settlement_policy_hardened as update_product_settlement_policy;
 pub use payments::*;
 pub use recovery::{
     enter_apc_recovery, execute_recovery_swap_adapter, initialize_recovery_pool,
@@ -28,6 +30,6 @@ pub use settlement_v2::{
     calculate_settlement_pex_obligation, calculate_settlement_quote_requirement,
     derive_settlement_source_split, execute_settlement_vault_funding, finalize_settlement,
     fund_direct_pex_settlement, initialize_product_settlement_policy,
-    initialize_settlement_policy, plan_settlement, update_product_settlement_policy,
+    initialize_settlement_policy, plan_settlement,
 };
 pub use vault::*;
